4aeca70b3296ee9efcb91084907e463847d0eb59
kalyanij967 <78337074+kalyanij967@users.noreply.github.com>
2023-07-17 13:08:14 +0530

    Update Pagination.js

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -1,61 +1,61 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react';
 import ReactPaginate from 'react-paginate';
 
-const Pagination = ({info,pageNumber,updatePageNumber}) => {
+const Pagination = ({ info, pageNumber, updatePageNumber }) => {
   let pageChange = (data) => {
     updatePageNumber(data.selected + 1);
   };
-  
-  let [width,setWidth]=useState(window.innerWidth);
+
+  let [width, setWidth] = useState(window.innerWidth);
 
   console.log(width);
 
-  let updateDimensions=()=>{
-    setWidth(window.innerWidth)
+  let updateDimensions = () => {
+    setWidth(window.innerWidth);
   };
 
-  useEffect(()=>{
-    window.addEventListener("resize",updateDimensions);
-    return ()=> window.removeEventListener("resize",updateDimensions);
-  },[]);
+  useEffect(() => {
+    window.addEventListener('resize', updateDimensions);
+    return () => window.removeEventListener('resize', updateDimensions);
+  }, []);
 
   return (
-  <>
-  <style jsx>
-    {`
-      @media (max-width:768 px){
-        .next,.prev{
-          display:none;
-        }
-        .pagination{
-          font-size:12px
-        }
-      }
-      @media (max-width:786 px){
-        .pagination{
-          font-size:14px ;
-        }
-      }
-    `}
-  </style>
-  <ReactPaginate 
-  className='pagination justify-content-center gap-4 my-4'
-  forcePage={pageNumber===1?0:pageNumber-1}
-  previousLabel="Prev"
-  previousClassName='btn btn-primary fs-5 prev'
-  nextLabel="Next"
-  nextClassName='btn btn-primary fs-5 next'
-  pageClassName='page-item'
-  pageLinkClassName='page-link'
-  activeClassName='active'
-  marginPagesDisplayed={width<576?1:2}
-  pageRangeDisplayed={width<576?1:2}
-  pageCount={info?.pages}
-  onPageChange={pageChange}
-  />
-  </>
+    <>
+      <style jsx>
+        {`
+          @media (max-width: 768px) {
+            .next,
+            .prev {
+              display: none;
+            }
+            .pagination {
+              font-size: 12px;
+            }
+          }
+          @media (max-width: 786px) {
+            .pagination {
+              font-size: 14px;
+            }
+          }
+        `}
+      </style>
+      <ReactPaginate
+        className="pagination justify-content-center gap-4 my-4"
+        forcePage={pageNumber === 1 ? 0 : pageNumber - 1}
+        previousLabel="Prev"
+        previousClassName="btn btn-primary fs-5 prev"
+        nextLabel="Next"
+        nextClassName="btn btn-primary fs-5 next"
+        pageClassName="page-item"
+        pageLinkClassName="page-link"
+        activeClassName="active"
+        marginPagesDisplayed={width < 576 ? 1 : 2}
+        pageRangeDisplayed={width < 576 ? 1 : 2}
+        pageCount={info?.pages}
+        onPageChange={pageChange}
+      />
+    </>
   );
-
 };
 
 export default Pagination;
